2f50717fbd06b8544e56a7b62242fce1355d34ce
Thomas <thomas@forgione.fr>
2026-03-06 11:53:55 +0100

    Prevent saving on 8000

--- prevent-saving.sh
+++ prevent-saving.sh
@@ -5,4 +5,4 @@
 #   - run `screen -ls` to see if screen is running,
 #   - `screen -r prevent-saving` to attach to prevent saving thread, and Ctrl+A and then Ctrl+D to detach again.
 screen -XS prevent-saving quit > /dev/null
-screen -S prevent-saving -d -m sh -c "while true; do echo "curl" && curl localhost:5000 > /dev/null 2>&1 && sleep 5s; done"
+screen -S prevent-saving -d -m sh -c "while true; do echo "curl" && curl localhost:8000 > /dev/null 2>&1 && sleep 5s; done"
