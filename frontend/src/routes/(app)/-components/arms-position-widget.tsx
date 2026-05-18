0ccbd5e5a785eb27979b312f1a91242e0c358b1a
Corentin Dominguez <corentin.dmz@live.fr>
2026-05-18 09:59:21 +0200

    chore: change arms position widget colors

--- frontend/src/routes/(app)/-components/arms-position-widget.tsx
+++ frontend/src/routes/(app)/-components/arms-position-widget.tsx
@@ -7,13 +7,13 @@ const ArmsPositionWidget = () => {
   const [openIncreaseArmsPositionDialog, setOpenIncreaseArmsPositionDialog] = useState(false);
   return (
     <div className="flex min-h-[32px] items-center rounded-lg">
-      <div className="flex h-full min-w-[70px] items-center justify-center gap-1 rounded-l-lg bg-gray-800 p-1 text-lg">
+      <div className="flex h-full min-w-[70px] items-center justify-center gap-1 rounded-l-lg bg-gray-200 p-1 text-lg">
         <span>{armsPosition?.emojiLeft}</span>
         <span>{armsPosition?.emojiRight}</span>
       </div>
       <button
         onClick={() => setOpenIncreaseArmsPositionDialog(true)}
-        className="h-full rounded-r-lg bg-red-200 p-1 px-3 py-1 text-sm font-medium text-red-700 transition-all duration-200 hover:bg-red-300"
+        className="h-full rounded-r-lg bg-red-900 p-1 px-3 py-1 text-sm font-medium text-gray-200 transition-all duration-200 hover:bg-red-800"
       >
         J'ai bougé les bras
       </button>
