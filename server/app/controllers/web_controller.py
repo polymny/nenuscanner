03595c8243904e3b633e57688225a5274130a335
Thomas Forgione <thomas@forgione.fr>
2026-06-05 14:18:20 +0200

    Python now serves react

--- server/app/controllers/web_controller.py
+++ server/app/controllers/web_controller.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file, send_from_directory
 
 from . import (
     acquisition_controller,
@@ -10,16 +10,18 @@ from . import (
     object_controller,
     test_controller,
 )
-from ... import db
 
 blueprint = Blueprint('routes', __name__)
 
 
 @blueprint.route('/')
 def index():
-    conn = db.get()
-    projects = db.Object.all_by_project(conn)
-    return render_template('index.html', projects=projects)
+    return send_file('../frontend/dist/index.html')
+
+
+@blueprint.route('/assets/<path:filename>')
+def asset(filename):
+    return send_from_directory('../frontend/dist/assets', filename)
 
 
 @blueprint.route('/restart')
@@ -39,4 +41,3 @@ blueprint.register_blueprint(acquisition_controller.blueprint, url_prefix='/acqu
 blueprint.register_blueprint(camera_controller.blueprint, url_prefix='/camera')
 blueprint.register_blueprint(leds_controller.blueprint, url_prefix='/leds')
 blueprint.register_blueprint(test_controller.blueprint, url_prefix='/test')
-
