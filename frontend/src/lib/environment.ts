f900c05528289fd0daeeed496a0e8a4150487ba3
Corentin Dominguez <corentin.dmz@live.fr>
2026-06-22 15:56:05 +0200

    fix(frontend): change Pi IP

--- frontend/src/lib/environment.ts
+++ frontend/src/lib/environment.ts
@@ -1,10 +1,10 @@
 const LOCAL_API_URL = 'http://127.0.0.1:5000';
-const PROD_API_URL = 'http://192.168.0.2:8000';
+const PROD_API_URL = 'http://192.168.64.8:8000';
 
 export const API_URL = import.meta.env.DEV ? LOCAL_API_URL : PROD_API_URL;
 
 const LOCAL_MEDIAMTX_STREAM_URL = 'http://127.0.0.1:8889/cam';
-const PROD_MEDIAMTX_STREAM_URL = 'http://192.168.0.2:8889/cam';
+const PROD_MEDIAMTX_STREAM_URL = 'http://192.168.64.8:8889/cam';
 
 /** MediaMTX WebRTC player URL. */
 export const MEDIAMTX_STREAM_URL = import.meta.env.DEV ? LOCAL_MEDIAMTX_STREAM_URL : PROD_MEDIAMTX_STREAM_URL;
