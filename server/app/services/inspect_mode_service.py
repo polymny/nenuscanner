f38e486516401b5fc37083c2415ffed9127baee9
Corentin Dominguez <corentin.dmz@live.fr>
2026-06-25 10:23:39 +0200

    chore(backend): turn on all leds when inspecting shutter

--- server/app/services/inspect_mode_service.py
+++ server/app/services/inspect_mode_service.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 
+from server.app.constants.leds import LEDS_COUNT
+
 from .camera_settings_service import get_current_camera_settings
 from .gphoto2_service import set_camera_setting
 from ..models.acquisition import Acquisition, AcquisitionStatus
@@ -38,11 +40,15 @@ def set_led_inspect_mode(session: Session, led_value: str) -> None:
 def set_shutter_speed_inspect_mode(session: Session, relative_value: float) -> None:
     _assert_no_running_acquisition(session)
     gpio_leds = leds.get()
-    gpio_leds.off()
+    gpio_leds.on()
 
     if config.CAMERA == 'real':
         camera_settings = get_current_camera_settings(session)
         target_shutter_speed = float(camera_settings.absolute_shutter_speed_value) * float(relative_value)
+
+        # TODO : correction temporaire
+        target_shutter_speed /= LEDS_COUNT
+
         set_camera_setting('shutterspeed', target_shutter_speed)
 
 
